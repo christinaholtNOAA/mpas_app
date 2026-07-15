f9e87422c2fd3648a420296b4aa084b26a44ed80
Christina Holt <Christina.Holt@noaa.gov>
2026-07-15 18:26:27 +0000

    WIP. Forecast seg fault with diff 0h and later lbcs.

--- load_wflow_modules.sh
+++ load_wflow_modules.sh
@@ -18,4 +18,5 @@ scrfunc_dir=$( dirname "${scrfunc_fp}" )
 module use $scrfunc_dir/modulefiles
 module load wflow_$1 > /dev/null 2>&1
 
-conda activate mpas_app
+#conda activate mpas_app
+conda activate DEV-uwtools
