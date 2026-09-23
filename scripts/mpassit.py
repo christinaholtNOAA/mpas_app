220670bd86536ff7ffe8c6ebd2fd8b852fde4fa8
Christina Holt <christina.holt@noaa.gov>
2026-09-23 10:41:08 -0600

    Use the mpassit driver instead of the old script.

---
+++ scripts/mpassit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+The run script for the MPASSIT postprocessing.
+"""
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from uwtools.api.mpas import MPAS
+
+from scripts.common import parse_args, run_component
+
+
+def main():
+    args = parse_args(lead_required=True)
+    run_component(
+        driver_class=MPASSIT,
+        config_file=args.config_file,
+        cycle=args.cycle,
+        leadtime=args.leadtime,
+        key_path=args.key_path,
+    )
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
